bbe5f3d4f5e082902c8b8f9748dffebe25ebe825
lucaolmrova <81082745+lucaolmrova@users.noreply.github.com>
2021-11-27 16:17:11 +0100

    logintest

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,10 +1,25 @@
-
-describe('Page', () => {
-
-    it('should open page', () => {
-        browser.reloadSession();
-        browser.url('/');
-        browser.pause(5000);
-    });
-    
+describe("EXTERMINATORS - HACKATHON TEST SUITE", () => {
+  it("Login from main page", () => {
+    browser.reloadSession();
+    browser.url("/");
+    const signIn = $(".hide_xs");
+    const emailField = $("#email");
+    const passwordField = $("#passwd");
+    const emailUser = "lolol@lol.com";
+    const passwordUser = "7mChDmhuaxsFmk9";
+    const submitButton = $("#SubmitLogin");
+    const userName = $("#user_info_acc");
+    signIn.click();
+    emailField.setValue(emailUser);
+    passwordField.setValue(passwordUser);
+    submitButton.click();
+    expect(userName.getText()).toEqual("Luca");
+  });
+  it.only("Main page room reservation", () => {
+    browser.reloadSession();
+    browser.url("/");
+    const hamburgerMenu = $(".nav_toggle");
+    const roomsLink = $('')
+    hamburgerMenu.click();
+  });
 });
